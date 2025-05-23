ccb2694203b0a21a037cb4494efc5a73c03578e9
Ankit Chobdar <ankitchobdar@gmail.com>
2025-05-23 18:05:34 -0400

    Fixed multiple button clicks + added logging of turns on UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import Log from "./components/Log";
 import Player from "./components/Player";
 import { useState } from "react";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X')
+    currentPlayer = 'O';
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  //const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+    //setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === 'X')
-        currentPlayer = 'O';
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
@@ -31,12 +38,12 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard 
-          onSelectSquare={handleSelectSquare} 
-          turns={gameTurns}   
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          turns={gameTurns}
         />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   )
 }
